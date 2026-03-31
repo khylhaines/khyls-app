32ae482f54b26305f7c9a1fe7e4d298f8e0bd905
khylhaines <khylhaines@gmail.com>
2026-03-31 22:19:00 +0100

    Update qa_groups.js

--- qa_groups.js
+++ qa_groups.js
@@ -1,19 +1,47 @@
+/* qa_groups.js */
+
 export const PIN_START_INTROS = {};
-export const QA_BY_GROUP = {};
+
+export const QA_PIN_OVERRIDES = {};
+
 export const RIDDLE_POOL = [];
-export const SPEED_POOL = { kid: [], teen: [], adult: [] };
-export const BATTLE_POOL = { kid: [], teen: [], adult: [] };
-export const FAMILY_POOL = { kid: [], teen: [], adult: [] };
-export const ACTIVITY_POOL = { kid: [], teen: [], adult: [] };
-export const ABBEY_GHOST_POOL = { kid: [], teen: [], adult: [] };
-export const GENERIC_GHOST_POOL = { kid: [], teen: [], adult: [] };
+
+export const ACTIVITY_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
+export const FAMILY_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
+export const SPEED_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
+export const BATTLE_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
+export const GENERIC_GHOST_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
+export const ABBEY_GHOST_POOL = {
+  kid: [],
+  teen: [],
+  adult: [],
+};
+
 export const DISCOVERY_PIN_CONTENT = {};
 export const GHOST_PIN_CONTENT = {};
 export const BOSS_PIN_CONTENT = {};
-export const HISTORY_MASTER_BANK = { kid: [], teen: [], adult: [] };
-
-export function getHistoryMasterBank(tier = "kid") {
-  return Array.isArray(HISTORY_MASTER_BANK[tier])
-    ? HISTORY_MASTER_BANK[tier]
-    : HISTORY_MASTER_BANK.kid;
-}
