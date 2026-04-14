1d92b6728ff19cebc320adf48dbdec55d78254bf
khylhaines <khylhaines@gmail.com>
2026-04-14 21:53:52 +0100

    Update app_wiring.js

--- system/app_wiring.js
+++ system/app_wiring.js
@@ -1,1 +1,29 @@
+export function wireApp({
+  playerSystem,
+  modalSystem,
+  shopBridge,
+  progressionSystem,
+  mapSystem,
+  notesSystem,
+}) {
+  // PLAYER
+  window.getActivePlayer = playerSystem.getActivePlayer;
+  window.renderHUD = playerSystem.renderHUD;
 
+  // MODALS
+  window.showModal = modalSystem.showModal;
+
+  // SHOP
+  window.ensureShopDefaults = shopBridge.ensureShopDefaults;
+
+  // PROGRESSION
+  window.getLevelFromXP = progressionSystem.getLevelFromXP;
+
+  // MAP
+  window.applyMapTheme = mapSystem.applyMapTheme;
+  window.createHeroIcon = mapSystem.createHeroIcon;
+  window.refreshHeroMarker = mapSystem.refreshHeroMarker;
+
+  // NOTES
+  window.renderHomeLog = notesSystem.renderHomeLog;
+}
