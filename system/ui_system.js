b37a8f47881595c21b9b322b94362dd88320b6e7
khylhaines <khylhaines@gmail.com>
2026-04-14 21:53:15 +0100

    Update ui_system.js

--- system/ui_system.js
+++ system/ui_system.js
@@ -1,1 +1,13 @@
+export function createUISystem({
+  renderShop,
+  renderHomeLog,
+}) {
+  function renderAllUI() {
+    renderShop?.();
+    renderHomeLog?.();
+  }
 
+  return {
+    renderAllUI,
+  };
+}
