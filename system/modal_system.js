7954cc11bee570552acda137aa700cab1e3759b8
khylhaines <khylhaines@gmail.com>
2026-04-14 21:50:12 +0100

    Update modal_system.js

--- system/modal_system.js
+++ system/modal_system.js
@@ -1,1 +1,24 @@
+export function createModalSystem() {
+  function hideAllModals() {
+    document.querySelectorAll(".full-modal").forEach((el) => {
+      el.style.display = "none";
+    });
+  }
 
+  function showModal(id) {
+    hideAllModals();
+    const el = document.getElementById(id);
+    if (el) el.style.display = "block";
+  }
+
+  function closeModal(id) {
+    const el = document.getElementById(id);
+    if (el) el.style.display = "none";
+  }
+
+  return {
+    hideAllModals,
+    showModal,
+    closeModal,
+  };
+}
