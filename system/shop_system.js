2a0b2f3026e80f488ce4bc63d4bbd08ebab48671
khylhaines <khylhaines@gmail.com>
2026-04-14 21:51:19 +0100

    Update shop_system.js

--- system/shop_system.js
+++ system/shop_system.js
@@ -1,1 +1,27 @@
+import { ensureDefaultOwnedInventory } from "../shop_system.js";
 
+export function createShopBridge({ getState, saveState }) {
+  function ensureShopDefaults() {
+    const state = getState();
+
+    const result = ensureDefaultOwnedInventory(
+      state.inventory,
+      state.purchasedItems
+    );
+
+    state.inventory = result.inventory;
+    state.purchasedItems = result.purchasedItems;
+
+    saveState?.();
+  }
+
+  function getInventoryCount(itemId) {
+    const state = getState();
+    return Number(state?.inventory?.[itemId] || 0);
+  }
+
+  return {
+    ensureShopDefaults,
+    getInventoryCount,
+  };
+}
