eb76e7b7a4453d1ab166f169660e7db9283ea46a
khylhaines <khylhaines@gmail.com>
2026-04-14 21:51:50 +0100

    Update adults_lock_system.js

--- system/adults_lock_system.js
+++ system/adults_lock_system.js
@@ -1,1 +1,27 @@
+export function createAdultLockSystem({ getState, saveState }) {
+  function isAdultUnlocked() {
+    return !!getState()?.adultUnlocked;
+  }
 
+  function unlockAdultMode(pin) {
+    if (pin === "1986") {
+      const state = getState();
+      state.adultUnlocked = true;
+      saveState?.();
+      return true;
+    }
+    return false;
+  }
+
+  function lockAdultMode() {
+    const state = getState();
+    state.adultUnlocked = false;
+    saveState?.();
+  }
+
+  return {
+    isAdultUnlocked,
+    unlockAdultMode,
+    lockAdultMode,
+  };
+}
